33ddfe540052f8ff9a619a03dbd31f09f25fefe6
Atul Shrestha <atulshrez@gmail.com>
2023-06-15 12:29:48 +0545

    changes to rec

--- pages/recommendations.tsx
+++ pages/recommendations.tsx
@@ -1,10 +1,28 @@
-import { Box, Divider, Text } from '@chakra-ui/layout';
+import { Box, Center, Divider, Text } from '@chakra-ui/layout';
+import { AiFillExclamationCircle } from 'react-icons/ai';
+import { BsDot } from 'react-icons/bs';
+import {
+    Icon,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    useDisclosure,
+    Button,
+} from '@chakra-ui/react';
 import { validateToken } from '../lib/auth';
 import prisma from '../lib/prisma';
 import ShowArtists from '../components/showArtists';
 import SongTable from '../components/songTable';
+import { useFavs } from '../lib/hooks';
 
-const Recommendations = ({ recArtists, recSongs }) => {
+const Recommendations = ({ recArtists, recSongs, myFavGenres }) => {
+    const { favSongs } = useFavs();
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const uniqueFavGenres = myFavGenres.filter((x, i, a) => a.indexOf(x) === i);
     return (
         <Box
             height="100%"
@@ -34,9 +52,69 @@ const Recommendations = ({ recArtists, recSongs }) => {
                 },
             }}
         >
-            <Text fontSize="4xl" color="schemeTwo.textColor">
-                For You...
-            </Text>
+            <Box display="flex" justifyContent="space-between">
+                <Text fontSize="4xl" color="schemeTwo.textColor">
+                    For You...
+                </Text>
+                <Button
+                    onClick={onOpen}
+                    background="schemeTwo.bodyPink"
+                    _hover={{ background: 'schemeTwo.bodyBlue' }}
+                >
+                    Details
+                </Button>
+
+                <Modal
+                    isOpen={isOpen}
+                    onClose={onClose}
+                    scrollBehavior="inside"
+                >
+                    <ModalOverlay />
+                    <ModalContent>
+                        <ModalHeader>Recommendation Details</ModalHeader>
+                        <ModalCloseButton />
+                        <ModalBody>
+                            {favSongs.length === 0 ? (
+                                <Center
+                                    border="2px"
+                                    borderColor="schemeTwo.bodyPink"
+                                    marginBottom="2rem"
+                                    borderRadius="0.4rem"
+                                    gap={3}
+                                    color="schemeTwo.bodyPink"
+                                    padding="0.6rem"
+                                >
+                                    <Icon as={AiFillExclamationCircle} />
+                                    <Text fontSize="md">
+                                        Having favourites leads to more tailored
+                                        recommnedation experience
+                                    </Text>
+                                </Center>
+                            ) : (
+                                <Box>
+                                    <Text fontSize="md">
+                                        Genres from favourited songs:{' '}
+                                    </Text>
+                                    <ul>
+                                        {uniqueFavGenres.map((genre) => (
+                                            <li>
+                                                <Icon as={BsDot} />
+                                                {genre}
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </Box>
+                            )}
+                        </ModalBody>
+
+                        <ModalFooter>
+                            <Button colorScheme="blue" mr={3} onClick={onClose}>
+                                Close
+                            </Button>
+                        </ModalFooter>
+                    </ModalContent>
+                </Modal>
+            </Box>
             <ShowArtists artists={recArtists} title="Recommended Artists" />
             <Divider />
             <Text paddingX="2rem" marginY="3rem" fontSize="3xl">
@@ -62,6 +140,8 @@ export const getServerSideProps = async ({ req }) => {
     }
     // const totalFavs = await prisma.favourite.findMany({});
 
+    let recSongs;
+    let recArtists;
     //* Get All The Favourites of All the Users
     const users = await prisma.user.findMany({
         select: {
@@ -90,6 +170,8 @@ export const getServerSideProps = async ({ req }) => {
         },
     });
 
+    console.log('My Favs:\n', myFavs);
+
     //* Get the Favourited Song IDs of current user
     const myFavIds = myFavs.map((myFav) => myFav.songId);
 
@@ -100,6 +182,10 @@ export const getServerSideProps = async ({ req }) => {
         .map((myFav) => myFav.song.artist.id)
         .filter((x, i, a) => a.indexOf(x) === i);
     console.log('myFavedArtistsIds:\n', myFavedArtistsIds);
+
+    //* For Modal
+    const otherUsers = users.filter((users) => users.id !== user.id);
+    console.log('Other Users:\n', otherUsers);
 
     //* Get favourites of the rest of the users excluding current user, could have done it earlier
     const otherUsersFavs = users
@@ -167,7 +253,7 @@ export const getServerSideProps = async ({ req }) => {
 
     console.log('songIdsViaGenres:\n', songIdsViaGenres);
 
-    //* Find the common song Ids between the songs gotten by other users and songs gotten via curren favourited songs' artists genre
+    //* Find the common song Ids between the songs gotten by other users and songs gotten via current favourited songs' artists genre
     const commonIds = filteredIds.filter((fid) =>
         songIdsViaGenres.includes(fid)
     );
@@ -192,46 +278,69 @@ export const getServerSideProps = async ({ req }) => {
         .map((a) => a.artistId)
         .filter((x, i, a) => a.indexOf(x) === i);
     console.log('reInforcedArtistsIds:\n', reInforcedArtistsIds);
+    if (myFavs.length === 0) {
+        const his = await prisma.history.findMany({});
+        const hisSongs = his.map((h) => h.songId);
+        const favs = await prisma.favourite.findMany({});
+        const favsSongs = favs.map((f) => f.songId);
+        const totalSongs = [...hisSongs, ...favsSongs];
+        const uniqueSongs = totalSongs.filter((x, i, a) => a.indexOf(x) === i);
 
-    // * These artists to rec
-    const recArtists = await prisma.artist.findMany({
-        where: {
-            OR: reInforcedArtistsIds.map((reid) => ({
-                id: reid,
-            })),
-        },
-        include: {
-            songs: {
-                include: {
-                    artist: true,
+        recSongs = await prisma.song.findMany({
+            where: {
+                OR: uniqueSongs.map((sid) => ({
+                    id: sid,
+                })),
+            },
+            include: {
+                artist: true,
+            },
+        });
+
+        recArtists = recSongs
+            .map((song) => song.artist)
+            .filter((x, i, a) => a.findIndex((item) => item.id === x.id) === i);
+    } else if (filteredIds.length === 0) {
+        recArtists = artistsViaGenres;
+        recSongs = artistsViaGenres.map((artist) => artist.songs).flat(1);
+    } else {
+        // * These artists to rec
+        recArtists = await prisma.artist.findMany({
+            where: {
+                OR: reInforcedArtistsIds.map((reid) => ({
+                    id: reid,
+                })),
+            },
+            include: {
+                songs: {
+                    include: {
+                        artist: true,
+                    },
                 },
             },
-        },
-    });
-    console.log(recArtists);
+        });
 
-    // * These songs to rec - take filteredId and exclude current favourited songs' Ids
-    const recSongsIds = filteredIds
-        .filter((x, i, a) => a.indexOf(x) === i)
-        .filter((filid) => !myFavIds.includes(filid));
+        // * These songs to rec - take filteredId and exclude current favourited songs' Ids
+        const recSongsIds = filteredIds
+            .filter((x, i, a) => a.indexOf(x) === i)
+            .filter((filid) => !myFavIds.includes(filid));
 
-    const recSongs = await prisma.song.findMany({
-        where: {
-            OR: recSongsIds.map((rsid) => ({
-                id: rsid,
-            })),
-        },
-        include: {
-            artist: true,
-        },
-    });
-
-    console.log(recSongs);
-
+        recSongs = await prisma.song.findMany({
+            where: {
+                OR: recSongsIds.map((rsid) => ({
+                    id: rsid,
+                })),
+            },
+            include: {
+                artist: true,
+            },
+        });
+    }
     return {
         props: {
             recArtists: JSON.parse(JSON.stringify(recArtists)),
             recSongs: JSON.parse(JSON.stringify(recSongs)),
+            myFavGenres: JSON.parse(JSON.stringify(myFavGenres)),
         },
     };
 };
